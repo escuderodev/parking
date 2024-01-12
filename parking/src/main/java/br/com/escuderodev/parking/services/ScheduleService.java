c1e180b285121627200afd687d210c8665173cec
escuderodev <escudero.ti@gmail.com>
2024-01-12 16:27:02 -0300

    feat: ScheduleService implementado

---
+++ parking/src/main/java/br/com/escuderodev/parking/services/ScheduleService.java
@@ -0,0 +1,22 @@
+package br.com.escuderodev.parking.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ScheduleService {
+
+    private EmailService emailService;
+
+    public ScheduleService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+//    @Scheduled(fixedDelay = 3300000, initialDelay = 3600000)
+    @Scheduled(fixedDelay = 1000, initialDelay = 1000)
+    public void senEmail() {
+        log.info("Enviando email...");
+    }
+}
